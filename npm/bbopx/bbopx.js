143af7c1360bd045956dc5ab09a019b4a806d955
SC <github.com@genkisugi.net>
2014-07-23 12:16:08 -0700

    npm bump

--- npm/bbopx/bbopx.js
+++ npm/bbopx/bbopx.js
@@ -4302,9 +4302,12 @@ bbopx.noctua.widgets.reporter = function(output_id){
 };
 
 // If it looks like we're in an environment that supports CommonJS
-// Modules 1.0, take the bbop namespace whole and export it. Otherwise
-// (browser environment, etc.), take no action and depend on the
-// global namespace.
+// Modules 1.0, take the bbop namespace whole and iteratively export
+// it. Otherwise (browser environment, etc.), take no action and
+// depend on the global namespace.
 if( typeof(exports) != 'undefined' ){
-    exports.bbopx = bbopx;
+    //exports.bbopx = bbopx;
+    bbop.core.each(bbopx, function(k, v){
+	exports[k] = v;
+    });
 }
