16bf6f1987b21c8ef08516b9f935d54b4f220f43
SC <github.com@genkisugi.net>
2015-04-07 17:00:27 -0700

    bump

--- external/bbop.js
+++ external/bbop.js
@@ -2015,7 +2015,7 @@ bbop.version.revision = "2.3.0";
  *
  * Partial version for this library: release (date-like) information.
  */
-bbop.version.release = "20150406";
+bbop.version.release = "20150407";
 /*
  * Package: logger.js
  * 
